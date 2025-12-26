be782bed4bb2183db86b3ba7b57cd76ebf4b55e7
Poporxr <poporxr@gmail.com>
2025-12-26 18:23:05 +0100

    implement api routes, server actions,and caching and the event detailspage

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,16 @@
 import EventCard from "@/components/EventCard";
 import ExploreBtn from "@/components/ExploreBtn";
-import events from "@/lib/constants";
+import { cacheLife } from "next/cache";
 
-const page = ( ) => {
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+
+const page = async ( ) => {
+  'use cache';
+  cacheLife('hours')
+  const response = await fetch(`${BASE_URL}/api/events`);
+
+  const {events} = await response.json();
+
   return(
     <section>
       <h1 className="text-center">The Hub of Every Dev <br /> Event You Can&apos;t Miss</h1>
@@ -13,8 +21,8 @@ const page = ( ) => {
         <h3>Featured Event</h3>
 
         <ul className="events">
-          {events.map((event) => {
-            return  (<li key={event.id} className="list-none">
+          {events && events.length > 0 && events.map((event: IEvent) => {
+            return  (<li key={event.slug} className="list-none">
               <EventCard {...event}/>
             </li>)
           })}
---
+++ components/BookEvent.tsx
@@ -0,0 +1,46 @@
+'use client';
+import { useState } from 'react';
+
+
+const BookEvent = () => {
+  const [email, setEmail] = useState('');
+  const [submitted, setSubmitted] = useState(false);
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    // Simulate form submission
+    setTimeout(() => {
+      setSubmitted(true);
+    }, 1000);
+  }
+
+
+  return (
+    <div id='book-event'>
+      {submitted ? (
+        <p className='text-sm'>Thank you for booking! A confirmation email has been sent to {email}.</p>
+      ) : (
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor='email' >Email Address</label>
+            <input
+              type='email'
+              id='email'
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder='Enter Your Email' />
+          </div>
+          <button
+            type='submit'
+            className='button-submit'
+            disabled={!email}
+          >
+            Book Now
+          </button>
+        </form>
+      )}
+    </div>
+  );
+};
+
+export default BookEvent;
